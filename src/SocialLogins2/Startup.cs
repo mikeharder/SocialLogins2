4f1fffd0eba06bf9a814f9e18b30ed8f4829f042
Mike Harder <mharder@microsoft.com>
2016-11-15 15:59:28 -0800

    Change DbContext from SQL to in-memory

--- src/SocialLogins2/Startup.cs
+++ src/SocialLogins2/Startup.cs
@@ -43,7 +43,7 @@ namespace SocialLogins2
             services.AddApplicationInsightsTelemetry(Configuration);
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseInMemoryDatabase());
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
